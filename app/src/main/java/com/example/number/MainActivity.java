ab98e87f6d7b3911471bbc47c21bde3e032f8bf2
mvkoroteev <mvkoroteev@fa.ru>
2019-03-28 12:27:17 +0300

    Bugfix

---
+++ app/src/main/java/com/example/number/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.number;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInfo = findViewById(R.id.textView);
+        etInput = findViewById(R.id.editText);
+        bControl = findViewById(R.id.button);
+
+    }
+
+    public void onClick(View view) {
+        int value = Integer.parseInt(etInput.getText().toString());
+        tvInfo.setText(getResources().getString(R.string.ahead));
+    }
+}
+
