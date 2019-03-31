0bd9f1ddf32af9e32ed10b19f7e5153b78963317
wasko3422 <panow.egor2018@gmail.com>
2019-03-31 20:02:31 +0300

    Added modes, buttons and other stuff

--- app/src/main/java/com/example/number/MainActivity.java
+++ app/src/main/java/com/example/number/MainActivity.java
@@ -6,12 +6,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Random rand;
     TextView tvInfo;
     EditText etInput;
     Button bControl;
+    Button ExitButton;
+    Button ModeButton;
+    int randomNumber;
+    Boolean hardMode = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +29,72 @@ public class MainActivity extends AppCompatActivity {
         etInput = findViewById(R.id.editText);
         bControl = findViewById(R.id.button);
 
+        rand = new Random();
+        randomNumber = rand.nextInt(100) + 1;
+        //tvInfo.setText(randomNumber);
+
+        ExitButton = findViewById(R.id.ExitButton);
+
+        ExitButton.setOnClickListener(new View.OnClickListener() {  //хз чё тут с форматированием атас полный
+                                          @Override
+                                          public void onClick(View v) {
+                                              finish();
+                                              System.exit(0);
+                                          }
+                                      }
+
+        );
+
+        ModeButton = findViewById(R.id.mode);
+
+        ModeButton.setOnClickListener(new View.OnClickListener() {  //same story
+                                          @Override
+                                          public void onClick(View v) {
+                                              if(!hardMode){
+                                                  randomNumber = rand.nextInt(10000000) + 1;
+                                                  hardMode = true;
+                                                  ModeButton.setText("Easy mode");
+                                                  tvInfo.setText("Теперь хард моуд - угадать от 1 до 10000000");
+                                              }else{
+                                                  randomNumber = rand.nextInt(100) + 1;
+                                                  hardMode = false;
+                                                  ModeButton.setText("Hard mode");
+                                                  tvInfo.setText("Обратно в легкий режим - от 1 до 100");
+                                              }
+                                          }
+                                      }
+
+        );
+
+
+
     }
 
     public void onClick(View view) {
+        String buff = etInput.getText().toString();
+        if (buff.length() == 0){  //если введено ничего
+            tvInfo.setText("Введите число!!!!!");
+            return;
+        }
         int value = Integer.parseInt(etInput.getText().toString());
-        tvInfo.setText(getResources().getString(R.string.ahead));
+
+        if(!hardMode) {
+            if (value < 0 || value > 100) {
+                tvInfo.setText("Число находится вне диапазона!!!!!");
+                return;
+            }
+        }else{
+            if (value < 0 || value > 10000000) {
+                tvInfo.setText("Число находится вне диапазона!!!!!");
+                return;
+            }
+        }
+
+        if (value == randomNumber){
+            tvInfo.setText(String.format("Попал число - %d", randomNumber));
+        }else {
+            tvInfo.setText(getResources().getString(R.string.ahead) + randomNumber);  //для проверки
+        }
     }
 }
 
